70fe4dd7594a96619a8da40643c1b86c8e5128b7
Round0000 <billybinche@gmail.com>
2021-11-23 12:36:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const plateauSchema = {};
+
+console.log(getComputedStyle(player).gridColumn);
+
+player.style.gridRow = getComputedStyle(case12).gridRow;
+player.style.gridColumn = getComputedStyle(case12).gridColumn;
