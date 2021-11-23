2b7f0fc80ddd3a58a0a4d519d68a5931dc368692
Round0000 <billybinche@gmail.com>
2021-11-23 20:13:11 +0100

    'commit'

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const players = {
   },
 };
 
+const events = {
+  1: "Départ : recevez 200€!",
+  2: "Boulevard de Belleville",
+  3: "Caisse de communauté",
+  4: "Rue Lecourbe",
+  5: "Impôts sur le revenu: payez 100€!",
+  6: "Gare Montparnasse",
+  7: "Rue de Vaugirard",
+}
+
 function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -28,33 +38,23 @@ function getRandom(min, max) {
 function placePlayer(plr, pos) {
   plr = players[plr];
 
-  console.log("placePlayer plr", plr);
-  console.log("placePlayer pos", pos);
-
   plr.currentPos = pos;
 
   let playerID = document.getElementById(plr.id);
   let posID = document.getElementById("case" + plr.currentPos);
 
-  console.log(playerID);
-  console.log(posID);
+  posID.scrollIntoView();
 
   playerID.style.gridRow = getComputedStyle(posID).gridRow;
   playerID.style.gridColumn = getComputedStyle(posID).gridColumn;
 
-  console.log(playerID);
+  console.log(plr.id + " placed at case " + plr.currentPos);
+  executeEvent(plr);
 }
-
-placePlayer(1, 1);
-placePlayer(2, 1);
-placePlayer(3, 1);
-placePlayer(4, 1);
 
 function movePlayer(plr) {
   let currPos = players[plr].currentPos;
   let roll = getRandom(1, 6);
-  console.log("currPos ---", currPos);
-  console.log("roll ---", roll);
 
   if (currPos + roll === 41) {
     placePlayer(plr, 1);
@@ -72,6 +72,15 @@ function movePlayer(plr) {
     placePlayer(plr, (currPos += roll));
   }
 }
+
+function executeEvent(plr) {
+  console.log("EVENT :", events[plr.currentPos]);
+}
+
+placePlayer(1, 1);
+placePlayer(2, 1);
+placePlayer(3, 1);
+placePlayer(4, 1);
 
 movePlayer1.addEventListener("click", () => {
   movePlayer(1);
